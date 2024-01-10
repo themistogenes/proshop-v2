9979bfc3441eb228bb1d52606568e7a21657ba0f
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-01-10 18:14:57 -0500

    Product Details Page

--- frontend/src/components/Rating.jsx
+++ frontend/src/components/Rating.jsx
@@ -2,7 +2,7 @@ import { FaStar, FaStarHalfAlt, FaRegStar } from 'react-icons/fa';
 
 const Rating = ( {value, text}) => {
   return (
-    <div class="rating">
+    <div className="rating">
       <span>
         { value >= 1 ? <FaStar /> : value >= 0.5 ? <FaStarHalfAlt /> : <FaRegStar />}
       </span>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,20 +12,20 @@ import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />}></Route>
+      <Route path="/product/:id" element={<ProductScreen />}></Route>
     </Route>
   )
 )
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
 
 reportWebVitals();
