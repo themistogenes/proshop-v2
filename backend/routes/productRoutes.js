4c8e5f402913cfa4347908700fafa00cb473da41
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-01-24 14:49:08 -0500

    Custom Error Middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -11,11 +11,12 @@ router.get('/', asyncHandler(async (req, res) => {
 router.get('/:id', asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
 
-  if (product) {
+  if(product) {
     return res.json(product);
+  } else {
+    res.status(404);
+    throw new Error('Resource not found');
   }
-
-  res.status(404).json({message: 'Product not found'});
 }))
 
 export default router;
