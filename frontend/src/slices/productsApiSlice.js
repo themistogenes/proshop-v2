f07cf216614cb9de5f89f2297e0c791a9ca00af3
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-01-24 17:26:05 -0500

    Fix mistake in productsApiSlice

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -2,14 +2,14 @@ import { PRODUCTS_URL } from '../constants';
 import { apiSlice } from './apiSlice';
 
 export const productsApiSlice = apiSlice.injectEndpoints({
-  endpoints: (builder) => {{
+  endpoints: (builder) => ({
     getProducts: builder.query({
       query: () => ({
         url: PRODUCTS_URL
       }),
       keepUnusedDataFor: 5
     })
-  }}
+  })
 });
 
 export const { useGetProductsQuery } = productsApiSlice;
