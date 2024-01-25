083b7b09e1fcad7b7143cdfce319717480cee250
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-01-25 15:48:37 -0500

    Loader and Message Components

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,5 +1,7 @@
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 const HomeScreen = () => {
@@ -7,25 +9,27 @@ const HomeScreen = () => {
 
   return (
     <>
-    {
-      isLoading ? (
-        <h2>Loading...</h2>
-      ) : error ? (
-        <div>{error?.data?.message || error.error}</div>
-      ) : (
-        <>
-          <h1>Latest Products</h1>
-          <Row>
-            { products.map((product) => (
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-              </Col>
-            ))}
-          </Row>
-        </>
+      {
+        isLoading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">
+            {error?.data?.message || error.error}
+          </Message>
+        ) : (
+          <>
+            <h1>Latest Products</h1>
+            <Row>
+              { products.map((product) => (
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                  <Product product={product} />
+                </Col>
+              ))}
+            </Row>
+          </>
 
-      )
-    }
+        )
+      }
     </>
   )
 }
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -2,6 +2,8 @@ import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
 
 const ProductScreen = () => {
@@ -11,73 +13,73 @@ const ProductScreen = () => {
 
   return (
     <>
-    <Link className="btn btn-light my-3" to="/">
-      Go Back
-    </Link>
-
-    { isLoading ? (
-      <h2>Loading...</h2>
-    ) : error ? (
-      <div>{ error?.data?.message || error.error }</div>
-    ) : (
-      <Row>
-      <Col md={5}>
-        <Image src={product.image} alt={product.name} fluid></Image>
-      </Col>
-      <Col md={4}>
-        <ListGroup variant="flush">
-          <ListGroup.Item>
-            <h3>{product.name}</h3>
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <Rating value={product.rating} text={`${product.numReviews} reviews`} />
-          </ListGroup.Item>
-          <ListGroup.Item>
-            Price: ${product.price}         
-          </ListGroup.Item>
-          <ListGroup.Item>
-            Description: {product.description}
-          </ListGroup.Item>
-        </ListGroup>
-      </Col>
-      <Col md={3}>
-        <Card>
-          <ListGroup>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
+      { isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">
+        {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <Row>
+        <Col md={5}>
+          <Image src={product.image} alt={product.name} fluid></Image>
+        </Col>
+        <Col md={4}>
+          <ListGroup variant="flush">
             <ListGroup.Item>
-              <Row>
-                <Col>
-                  Price:
-                </Col>
-                <Col>
-                  <strong>${product.price}</strong>
-                </Col>
-              </Row>
+              <h3>{product.name}</h3>
             </ListGroup.Item>
             <ListGroup.Item>
-              <Row>
-                <Col>
-                  Status:
-                </Col>
-                <Col>
-                  <strong>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong>
-                </Col>
-              </Row>
+              <Rating value={product.rating} text={`${product.numReviews} reviews`} />
             </ListGroup.Item>
             <ListGroup.Item>
-              <Button
-                className='btn-block'
-                type='button'
-                disabled={product.countInStock === 0}
-              >
-                Add to Cart
-              </Button>
+              Price: ${product.price}         
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Description: {product.description}
             </ListGroup.Item>
           </ListGroup>
-        </Card>
-      </Col>
-      </Row>
-    ) }
-    
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup>
+              <ListGroup.Item>
+                <Row>
+                  <Col>
+                    Price:
+                  </Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>
+                    Status:
+                  </Col>
+                  <Col>
+                    <strong>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  className='btn-block'
+                  type='button'
+                  disabled={product.countInStock === 0}
+                >
+                  Add to Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+        </Row>
+      ) }
     </>
   )
 }
