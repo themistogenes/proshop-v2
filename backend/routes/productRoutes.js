86722915b93074b36a9f608bcc5e4ce25d3a3c07
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-01-31 12:16:10 -0500

    User Routes and Controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,8 +2,9 @@ import express from 'express';
 const router = express.Router();
 import { getProducts, getProductById } from '../controllers/productController.js';
 
-router.route('/').get(getProducts);
+// /api/products
 
+router.route('/').get(getProducts);
 router.route('/:id').get(getProductById);
 
 export default router;
