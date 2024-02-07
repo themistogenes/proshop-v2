3cbc425191b43d5290514357e2a54eab6d9510f5
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-02-07 11:05:28 -0500

    Shipping Screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,6 +18,7 @@ import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -27,6 +28,7 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CartScreen />}></Route>
       <Route path="/login" element={<LoginScreen />}></Route>
       <Route path="/register" element={<RegisterScreen />}></Route>
+      <Route path="/shipping" element={<ShippingScreen />}></Route>
     </Route>
   )
 )
