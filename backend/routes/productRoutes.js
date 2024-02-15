d12e1706e517175dba4e5da061b87aa176345566
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-02-15 11:48:46 -0500

    Create Product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
 const router = express.Router();
-import { getProducts, getProductById } from '../controllers/productController.js';
+import { getProducts, getProductById, createProduct } from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 // /api/products
 
-router.route('/').get(getProducts);
+router.route('/')
+  .get(getProducts)
+  .post(protect, admin, createProduct);
 router.route('/:id').get(getProductById);
 
 export default router;
