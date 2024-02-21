835743f208a65bf418e68bbe9f0b6824dc12abcb
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-02-21 11:36:38 -0500

    Upload Product Image - Frontend

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -8,7 +8,7 @@ const storage = multer.diskStorage({
     cb(null, 'uploads/'); // null is for an error
   },
   filename(req, file, cb) {
-    cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}}`)
+    cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
   }
 })
 
--- frontend/src/screens/admin/ProductEditScreen.jsx
+++ frontend/src/screens/admin/ProductEditScreen.jsx
@@ -5,7 +5,11 @@ import Message from '../../components/Message';
 import Loader from '../../components/Loader';
 import FormContainer from '../../components/FormContainer';
 import { toast } from 'react-toastify';
-import { useUpdateProductMutation, useGetProductDetailsQuery } from '../../slices/productsApiSlice';
+import { 
+  useUpdateProductMutation, 
+  useGetProductDetailsQuery,
+  useUploadProductImageMutation
+} from '../../slices/productsApiSlice';
 
 const ProductEditScreen = () => {
   const { id: productId } = useParams();
@@ -21,6 +25,8 @@ const ProductEditScreen = () => {
   const { data: product, isLoading, refetch, error } = useGetProductDetailsQuery(productId);
 
   const [updateProduct, { isLoading: loadingUpdate }] = useUpdateProductMutation();
+
+  const [uploadProductImage, { isLoading: loadingUpload }] = useUploadProductImageMutation();
 
   const navigate = useNavigate();
 
@@ -58,6 +64,18 @@ const ProductEditScreen = () => {
     }
   }
 
+  const uploadFileHandler = async (e) => {
+    const formData = new FormData();
+    formData.append('image', e.target.files[0]);
+    try {
+      const res = await uploadProductImage(formData).unwrap();
+      toast.success(res.message);
+      setImage(res.image);
+    } catch (err) {
+      toast.error(err?.data?.message || err.error);
+    }
+  }
+
   return (
     <>
       <Link to="/admin/productlist" className="btn btn-light my-3">
@@ -87,7 +105,22 @@ const ProductEditScreen = () => {
                   onChange={(e) => setPrice(e.target.value)}
                 ></Form.Control>
               </Form.Group>
-              {/* IMAGE INPUT PLACEHOLDER */}
+              <Form.Group controlId="image" className="my-2">
+                <Form.Label>Image</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter image url"
+                  value={image}
+                  onChange={(e) => setImage}
+                >    
+                </Form.Control>
+                <Form.Control
+                  type="file"
+                  label="Choose file"
+                  onChange={uploadFileHandler}
+                >
+                </Form.Control>
+              </Form.Group>
               <Form.Group controlId="brand" className="my-2">
                 <Form.Label>Brand</Form.Label>
                 <Form.Control
